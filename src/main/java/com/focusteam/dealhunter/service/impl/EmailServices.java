6181cad59c76090cb61eb854b63f1a91941e8d99
tienbac <tienbac250496@gmail.com>
2019-10-15 10:12:57 +0700

    Update variable in mail form html

--- src/main/java/com/focusteam/dealhunter/service/impl/EmailServices.java
+++ src/main/java/com/focusteam/dealhunter/service/impl/EmailServices.java
@@ -2,5 +2,5 @@ package com.focusteam.dealhunter.service.impl;
 
 public interface EmailServices {
     void sendSimpleMessage(String to, String subject, String text);
-    void sendMessageWithAttachment(String to, String subject, String fullName, String callbackUrl);
+    void sendMessageWithAttachment(String to, String subject, String fullName, String callbackUrl, String message);
 }
